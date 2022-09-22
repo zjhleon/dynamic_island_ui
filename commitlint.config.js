ff7bc4389b7917da7b8cfc940787143f129c18e0
zhangjiahao <zhangjiahao@bianfeng.com>
2022-09-22 17:20:35 +0800

    test

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,1 @@
-/*
- * @Descripttion:
- * @Author: zhangjiahao
- * @Date: 2022-09-22 17:10:27
- * @LastEditors: zhangjiahao
- * @LastEditTime: 2022-09-22 17:10:33
- */
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {extends: ['@commitlint/config-conventional']}
