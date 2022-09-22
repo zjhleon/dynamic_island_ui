6a8a7a5bc472a667a021c1b4747257a318d7b6cc
zhangjiahao <zhangjiahao@bianfeng.com>
2022-09-22 17:10:45 +0800

    test

---
+++ commitlint.config.js
@@ -0,0 +1,8 @@
+/*
+ * @Descripttion:
+ * @Author: zhangjiahao
+ * @Date: 2022-09-22 17:10:27
+ * @LastEditors: zhangjiahao
+ * @LastEditTime: 2022-09-22 17:10:33
+ */
+module.exports = { extends: ['@commitlint/config-conventional'] };
--- src/SFCButton.vue
+++ src/SFCButton.vue
@@ -3,10 +3,11 @@
  * @Author: zhangjiahao
  * @Date: 2022-09-21 16:06:41
  * @LastEditors: zhangjiahao
- * @LastEditTime: 2022-09-22 16:20:59
+ * @LastEditTime: 2022-09-22 17:09:15
 -->
 <template>
   <button>SFC Button</button>
+  <div>1</div>
 </template>
 
 <script lang="ts">
