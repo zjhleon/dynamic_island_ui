c084c7e4148c6539b5d1ddb427bfe53b1c759785
zhangjiahao <zhangjiahao@bianfeng.com>
2022-09-23 14:20:42 +0800

    feat: 添加github action

--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,48 @@
+/*
+ * @Descripttion:
+ * @Author: zhangjiahao
+ * @Date: 2022-09-21 16:03:15
+ * @LastEditors: zhangjiahao
+ * @LastEditTime: 2022-09-23 10:27:53
+ */
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import Unocss from "./config/unocss";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import Unocss from './config/unocss';
 
 const rollupOptions = {
-
-    external: ["vue", "vue-router"],
-    output: {
-        assetFileNames: `assets/[name].css`,
-      globals: {
-        vue: "Vue",
-      },
-    },
-  };
+  external: ['vue', 'vue-router'],
+  output: {
+    assetFileNames: `assets/[name].css`,
+    globals: {
+      vue: 'Vue'
+    }
+  }
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-        vue(), 
-        vueJsx(),
-        Unocss()
-    ],
+  plugins: [vue(), vueJsx(), Unocss()],
   build: {
     rollupOptions,
-    minify:false,
+    minify: false,
     cssCodeSplit: true,
+    sourcemap: true,
+    brotliSize: true,
     lib: {
-      entry: "./src/entry.ts",
-      name: "dynamic_island_ui",
-      fileName: "dynamic_island_ui",
+      entry: './src/entry.ts',
+      name: 'dynamic_island_ui',
+      fileName: 'dynamic_island_ui',
       // 导出模块格式
-      formats: ["esm", "umd","iife"],
-    },
+      formats: ['esm', 'umd', 'iife']
+    }
   },
   test: {
     globals: true,
     environment: 'happy-dom',
     transformMode: {
-        web: [/.[tj]sx$/]
+      web: [/.[tj]sx$/]
     }
   }
-})
+});
