908139a255f9bd4e931df473d16faf663d371fab
chihuosheng <zhangjh@gancao.com>
2024-04-26 14:14:13 +0800

    feat: c

--- src/entry.ts
+++ src/entry.ts
@@ -3,15 +3,12 @@
  * @Author: zhangjiahao
  * @Date: 2022-09-21 16:15:45
  * @LastEditors: 张佳浩
- * @LastEditTime: 2024-04-26 14:11:41
+ * @LastEditTime: 2024-04-26 14:13:41
  */
 import { App } from 'vue';
 import JSXButton from './JSXButton';
 import SFCButton from './SFCButton.vue';
 import MyButton from './button';
-console.log('1111');
-console.log('2222');
-console.log('333');
 
 //导出单独组件
 export { JSXButton, SFCButton, MyButton };
