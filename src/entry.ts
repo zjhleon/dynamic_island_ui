7b30b43db127e7b692608859014f3455b60ce7b4
chihuosheng <zhangjh@gancao.com>
2024-04-26 14:07:14 +0800

    test: test

--- src/entry.ts
+++ src/entry.ts
@@ -2,13 +2,15 @@
  * @Descripttion:
  * @Author: zhangjiahao
  * @Date: 2022-09-21 16:15:45
- * @LastEditors: zhangjiahao
- * @LastEditTime: 2022-09-22 15:53:46
+ * @LastEditors: 张佳浩
+ * @LastEditTime: 2024-04-26 14:06:53
  */
 import { App } from 'vue';
 import JSXButton from './JSXButton';
 import SFCButton from './SFCButton.vue';
 import MyButton from './button';
+console.log('======test');
+
 
 //导出单独组件
 export { JSXButton, SFCButton, MyButton };
