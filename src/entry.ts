4ce2bd4e7caba47d74adca6845ca23a9bd88d7be
chihuosheng <zhangjh@gancao.com>
2024-05-07 11:23:06 +0800

    Revert "test: 测试提交1"
    This reverts commit bc716d49725db8220df39564c9d7cfcc7bde031d.

--- src/entry.ts
+++ src/entry.ts
@@ -3,13 +3,12 @@
  * @Author: zhangjiahao
  * @Date: 2022-09-21 16:15:45
  * @LastEditors: 张佳浩
- * @LastEditTime: 2024-05-07 11:22:16
+ * @LastEditTime: 2024-04-26 14:13:41
  */
 import { App } from 'vue';
 import JSXButton from './JSXButton';
 import SFCButton from './SFCButton.vue';
 import MyButton from './button';
-console.log('test11111');
 
 //导出单独组件
 export { JSXButton, SFCButton, MyButton };
